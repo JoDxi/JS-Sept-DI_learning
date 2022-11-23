9815c47dec6adb8eee1e94e7c6a308a39bb4af4b
JoDxi <100140729+JoDxi@users.noreply.github.com>
2022-11-23 22:32:59 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,87 @@
+
+// alert('hello')
+// let a = 1;
+
+// let userName;
+
+// console.log(userName)
+
+
+// let sentence = "I like apples";
+//
+// console.log(sentence.indexOf("orange"))
+// // console.log(sentence.length)
+
+
+// let isBoss = confirm("Are you the boss?");
+// alert(isBoss); // true if OK is pressed
+
+
+// console.log(7%4)
+
+// console.log(typeof (3 + 4 + '5'));
+//
+// let sentence = ["my","favorite","color","is","blue"];
+//
+// console.log(sentence.join(" "))
+
+let str1 = "mix";
+let str2 = "pod";
+
+// console.log(str1.slice(0,2))
+// console.log(str2.slice(0,2))
+//
+// let str1x = str1.slice(0,2)
+// let str2x = str2.slice(0,2)
+//
+// console.log(str1.replace(str1x, str2x))
+// console.log(str2.replace(str2x, str1x))
+// console.log(str1)
+//
+// let a = 5
+// let b = 2
+// let temp = a;
+//
+// a = b
+// b = temp
+// console.log(a, b)
+
+//
+// let fruit = 'Papayas';
+//
+// switch (fruit) {
+//     case 'Oranges':
+//         console.log('Oranges are $0.59 a pound.');
+//         break;
+//     case 'Mangoes':
+//     case 'Papayas':
+//         console.log('Mangoes and papayas are $2.79 a pound.');
+//         // expected output: "Mangoes and papayas are $2.79 a pound."
+//         break;
+//     default:
+//         console.log('Sorry, we are out of ' + fruit + '.');// }
+
+
+let x = 7
+let y = 25;
+let g = 3
+
+// if (x > y && x > g) {
+//     console.log("x is bigger")
+// } else if (y > x && y> g) {
+//     console.log("y is bigger")
+// } else {
+//     console.log("g is bigger")
+// }
+
+if (x > y) {
+    if (x > g) {
+        console.log("x is bigger")
+    } else {
+        console.log("g is bigger")
+    }
+} else if (y > g) {
+    console.log(" y is bigger")
+} else {
+    console.log("g is bigger")
+}
